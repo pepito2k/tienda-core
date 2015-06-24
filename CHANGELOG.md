dbd7d201a5c60bdcdb380469b704c17c7c1202d6
Gonzalo Robaina <gonzalor@gmail.com>
2015-06-24 01:23:24 -0300

    Fix order statistics bug in dashboard

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This document outlines key changes which are introduced in each version. The full commit history can be found [on GitHub](http://github.com/pepito2k/tienda-core).
 
+## v2.0.2
+
+* Fixed order statistics bug in Dashboard
+
 ## v2.0.1
 
 * Removed redundant dependencies
--- app/controllers/tienda/dashboard_controller.rb
+++ app/controllers/tienda/dashboard_controller.rb
@@ -7,11 +7,11 @@ module Tienda
       @last_30_days_orders = {}
       end_date = Date.today
       start_date = end_date - 30.days
-      last_30_days_orders = Tienda::Order.group('DATE(created_at)').where(created_at: start_date..end_date).count
+      last_30_days_orders = Tienda::Order.received.group('DATE(created_at)').where(created_at: start_date..end_date).count
       (start_date..end_date).each do |day|
         @last_30_days_orders[day] = last_30_days_orders[day] || 0
       end
-      @sales_by_category = Tienda::Order.joins(products: [:product_category]).where(created_at: start_date..end_date).group('tienda_product_categories.name').count
+      @sales_by_category = Tienda::Order.received.joins(products: [:product_category]).where(created_at: start_date..end_date).group('tienda_product_categories.name').count
       @users_count = Tienda::User.count
     end
 
