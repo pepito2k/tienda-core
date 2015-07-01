25dfc87c1db958a3accc425e4e7a21683b75481a
Gonzalo Robaina <gonzalor@gmail.com>
2015-06-30 22:18:50 -0300

    Fix carrierwave config for test env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,17 @@
 CarrierWave.configure do |config|
-  AWS_CONFIG = Rails.application.secrets.aws_config
+  unless Rails.env.test?
+    AWS_CONFIG = Rails.application.secrets.aws_config
 
-  config.storage    = :aws
-  config.aws_bucket = AWS_CONFIG['s3_bucket_name']
-  config.aws_acl    = :'public-read'
-  config.asset_host = 'https://cdn.masinfinitocasa.com'
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+    config.storage    = :aws
+    config.aws_bucket = AWS_CONFIG['s3_bucket_name']
+    config.aws_acl    = :'public-read'
+    config.asset_host = 'https://cdn.masinfinitocasa.com'
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
-  config.aws_credentials = {
-    access_key_id:     AWS_CONFIG['s3_key'],
-    secret_access_key: AWS_CONFIG['s3_secret'],
-    region:            AWS_CONFIG['s3_region']
-  }
+    config.aws_credentials = {
+      access_key_id:     AWS_CONFIG['s3_key'],
+      secret_access_key: AWS_CONFIG['s3_secret'],
+      region:            AWS_CONFIG['s3_region']
+    }
+  end
 end
